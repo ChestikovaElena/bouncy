fbfdeb3008728a6a4be0dd7e68bb9db6c49beeb7
Chestikova Elena <Chestikova.elena@gmail.com>
2020-10-18 19:41:04 +0500

    Add change of the text on Read More

--- src/js/more.js
+++ src/js/more.js
@@ -1,4 +1,6 @@
 document.getElementById("header-more-button").onclick = function() {
     var more_content = document.getElementById("header-more__content");
     more_content.style.display = (more_content.style.display == 'block') ? 'none' : 'block';
+    var more_image = document.getElementById("header-more-image");
+    more_image.src = (more_content.style.display == 'block') ? "../src/icons/more_up.png" : "../src/icons/more.png";
 }
--- src/js/promo_more.js
+++ src/js/promo_more.js
@@ -1,4 +1,7 @@
-document.getElementById("button_more").onclick = function() {
-    var more_content = document.getElementById("promo-more__content");
-    more_content.style.display = (more_content.style.display == 'block') ? 'none' : 'block';
+document.getElementById("promo_button_more").onclick = function() {
+    var promo_more_content = document.getElementById("promo-more__content");
+    promo_more_content.style.display = (promo_more_content.style.display == 'block') ? 'none' : 'block';
+    promo_more_button = document.getElementById("promo_button_more");
+    promo_more_button.innerText = (promo_more_content.style.display == 'block') ? "Hide" : "Read More";
+    promo_more_button.style.backgroundColor = (promo_more_content.style.display == 'block') ? '#19bd9a' : '#fff';
 }
